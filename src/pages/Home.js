111731810d82a31f7e68516becfd5fb8b9fadec5
Lukyianenko <110479686+Lukyianenko@users.noreply.github.com>
2023-04-26 20:42:26 +0300

    end

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,12 @@
-// import imge from "../images/1616699300_58-p-zhdun-krasivo-61.jpg";
+import imge from "../images/1616699300_58-p-zhdun-krasivo-61.jpg";
 export default function Home() {
     return (
       <div>
         <h1>
-        Phone book welcome page{' '}
+        Tweets welcome page{' '}
         <br/>
           <span role="img" aria-label="Greeting icon">
-            {/* <img src={imge} alt="wait" width="600" /> */}
+            <img src={imge} alt="wait" width="600" />
           </span>
         </h1>
       </div>
