7f7be2dbc5c46280258bbfd7b26ddaef82d10b2d
Lukyianenko <110479686+Lukyianenko@users.noreply.github.com>
2023-04-26 17:49:24 +0000

    Deploying to gh-pages from @ Lukyianenko/tech-order-followers@a357dfa6cf9de3558ca2a8383c4ffa7359e523b6 🚀

---
+++ static/js/397.85be2143.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunktech_order_followers=self.webpackChunktech_order_followers||[]).push([[397],{2388:function(e,r,i){i.r(r),i.d(r,{default:function(){return t}});var s=i.p+"static/media/1616699300_58-p-zhdun-krasivo-61.79698d4e4030a263d6e0.jpg",n=i(3329);function t(){return(0,n.jsx)("div",{children:(0,n.jsxs)("h1",{children:["Tweets welcome page"," ",(0,n.jsx)("br",{}),(0,n.jsx)("span",{role:"img","aria-label":"Greeting icon",children:(0,n.jsx)("img",{src:s,alt:"wait",width:"600"})})]})})}}}]);
+//# sourceMappingURL=397.85be2143.chunk.js.map
