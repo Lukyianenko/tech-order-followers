a357dfa6cf9de3558ca2a8383c4ffa7359e523b6
Lukyianenko <110479686+Lukyianenko@users.noreply.github.com>
2023-04-26 20:48:13 +0300

    fix bugs

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,22 +1,11 @@
-import { useEffect, lazy } from "react";
+import { lazy } from "react";
 import { Route, Routes } from 'react-router-dom';
-import { useDispatch, useSelector } from "react-redux/es/exports";
-import PropTypes from 'prop-types';
 import { Layout } from './Layout';
-import { PrivateRoute } from './PrivateRoute';
-// import { refreshUser } from 'redux/auth/operations';
-import { selectIsRefreshing } from '../redux/selectors';
 
 const HomePage = lazy(() => import('../pages/Home'));
 const TweetsPage = lazy(() => import('../pages/Users'));
 
 export const App = () => {
-  // const dispatch = useDispatch();
-  // const isRefreshing = useSelector(selectIsRefreshing);
-
-  // useEffect(() => {
-  //   dispatch(refreshUser());
-  // }, [dispatch]);
 
   return (
     <Routes>
@@ -30,13 +19,3 @@ export const App = () => {
   );
 };
 
-
-// App.propTypes = {
-//   state: PropTypes.arrayOf(PropTypes.exact({
-//     contacts: PropTypes.arrayOf(PropTypes.exact({
-//       id: PropTypes.string,
-//       name: PropTypes.string,
-//       number: PropTypes.string,
-//     })),
-//   })),
-// }
